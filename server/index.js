f1d88e384ef4e47b6d0d4a309bb1ff0b6c6f0ab9
flavioespinoza <flavio.espinoza@gmail.com>
2019-09-03 08:21:39 -0600

    Drone .env missing test

--- server/index.js
+++ server/index.js
@@ -14,16 +14,68 @@ const request = require("request");
 const log = require("ololog");
 
 const PORT = process.env.HOST || 5000;
+const jotform_api_key = process.env.JOTFORM_API_KEY;
+// const jotform_api_key = "e6271df2300c9d7c4e5d3f6aba33ec1c";
 
 const app = new Koa();
 const loginRoute = new Router();
 
+const _req_claim = async options => {
+  return new Promise(async resolve => {
+    request(options, async (error, response, body) => {
+      if (error) throw new Error(error);
+      let _body = JSON.parse(body);
+      console.log(_body);
+      resolve({
+        status_code: response.statusCode,
+        response_headers: response.headers,
+        body: _body
+      });
+    });
+  });
+};
+
 loginRoute.post("/api/openid-login", async (ctx, next) => {
   log.lightYellow(ctx.query);
   const email = ctx.query.email;
+
+	log.lightBlue("jotform_api_key");
+  log.lightBlue("jotform_api_key");
+  log.lightBlue("jotform_api_key");
+  log.lightBlue("jotform_api_key");
+  log.blue("jotform_api_key", jotform_api_key);
+  log.lightBlue("jotform_api_key");
+  log.lightBlue("jotform_api_key");
+  log.lightBlue("jotform_api_key");
+  log.lightBlue("jotform_api_key");
+
+
+  // const submission_id = 4428351239413163702;
+  // const jotform_uri = `https://hipaa-api.jotform.com/submission/${submission_id}`;
+  // const options = {
+  // 	method: "GET",
+  // 	url: jotform_uri,
+  // 	qs: { apikey: jotform_api_key }
+  // };
+  // const claim = await _req_claim(options);
+  // const answers_all = claim.body.content.answers;
+  // const answers = [];
+  // _.each(answers_all, (obj, key) => {
+  // 	if (obj.answer) {
+  // 		answers.push(obj);
+  // 	}
+  // });
+
+  // log.magenta(answers)
+
   ctx.body = {
     success: true,
-    email: email
+    email: email,
+    claim: {
+    	submission_id: 'claim.body.content.id',
+    	answers: 'answers',
+    	answers_all: 'answers_all'
+    }
   };
   await next();
 });

5046cd501b43862c8f638cbe73484678dd3a4201
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-04 19:14:00 -0600

    brew-bash

--- sync-examples.sh
+++ sync-examples.sh
@@ -1,59 +1,26 @@
-green='\033[32m'
-end_green='\033[0m'
-            
-red='\033[31m'
-end_red='\033[0m'
-            
-yellow='\033[33m'
-end_yellow='\033[0m'
-      
-blue='\033[34m'  
-end_blue='\033[0m'
+cd ..
 
-cyan='\033[36m'
-end_cyan='\033[0m'
+rm -rf tempest--*
 
-# vars -------
-timestamp=$(date +%s)
+git clone https://github.com/flavioespinoza/sync-docs.git tempest--${timestamp}
 
+cp docker-react-node/helm tempest--${timestamp}
 
-echo -e "${green}starting sync${end_green}"
+cd tempest--${timestamp}
 
-# curl https://github.com/github/hub/releases/download/v2.14.2/hub-linux-386-2.14.2.tgz 
+git checkout -b tempest--${timestamp}
 
-git clone https://github.com/github/hub.git
+git add .
 
-cd hub
+git commit 'Sync-Docs - Copy Examples'
 
-make install
+git push -u origin tempest--${timestamp}
 
-hub --version
+hub pull-request -m "Sync-Docs: tempest--${timestamp}"
 
-# cd ..
+cd ..
 
-# rm -rf tempest--*
-
-# git clone https://github.com/flavioespinoza/sync-docs.git tempest--${timestamp}
-
-# cp docker-react-node/helm tempest--${timestamp}
-
-# cd tempest--${timestamp}
-
-# git checkout -b tempest--${timestamp}
-
-# git add .
-
-# git commit 'Sync-Docs - Copy Examples'
-
-# git push -u origin tempest--${timestamp}
-
-# hub pull-request -m "Sync-Docs: tempest--${timestamp}"
-
-# cd ..
-
-# rm -rf tempest--${timestamp}
-
-# echo -e "${green}sync complete${end_green}"
+rm -rf tempest--${timestamp}
 
 # if [ -z "$(git status --porcelain)" ]; then 
 #   # Working directory clean

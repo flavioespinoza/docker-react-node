f5abfd304078c403ef06c459c832738ea1d3c4d9
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-04 18:08:27 -0600

    hub

---
+++ sync-examples.sh
@@ -0,0 +1,15 @@
+green='\033[32m'
+end_green='\033[0m'
+            
+red='\033[31m'
+end_red='\033[0m'
+            
+yellow='\033[33m'
+end_yellow='\033[0m'
+      
+blue='\033[34m'  
+end_blue='\033[0m'
+
+cyan='\033[36m'
+end_cyan='\033[0m'
+

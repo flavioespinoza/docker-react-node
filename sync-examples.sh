160710ca7fa5500b5e6368a21ed0ef4bc8fb81ee
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-04 18:13:08 -0600

    stuff

--- sync-examples.sh
+++ sync-examples.sh
@@ -13,3 +13,44 @@ end_blue='\033[0m'
 cyan='\033[36m'
 end_cyan='\033[0m'
 
+# vars -------
+timestamp=$(date +%s)
+
+
+echo -e "${green}starting sync${end_green}"
+
+curl -fsSL https://github.com/github/hub/raw/master/script/get | bash -s 2.14.1
+
+hub --version
+
+cd ..
+
+rm -rf tempest--*
+
+git clone https://github.com/flavioespinoza/sync-docs.git tempest--${timestamp}
+
+cp docker-react-node/helm tempest--${timestamp}
+
+cd tempest--${timestamp}
+
+git checkout -b tempest--${timestamp}
+
+git add .
+
+git commit 'Sync-Docs - Copy Examples'
+
+git push -u origin tempest--${timestamp}
+
+hub pull-request -m "Sync-Docs: tempest--${timestamp}"
+
+cd ..
+
+rm -rf tempest--${timestamp}
+
+echo -e "${green}sync complete${end_green}"
+
+# if [ -z "$(git status --porcelain)" ]; then 
+#   # Working directory clean
+# else 
+#   # Uncommitted changes
+# fi

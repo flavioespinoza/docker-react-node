57ec4d06d870f2b69825e9ce36f8ae0bdec8c0f3
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-04 20:56:25 -0600

    changes

--- sync-examples.sh
+++ sync-examples.sh
@@ -1,29 +1,34 @@
 
+# the root_source_dir contain the files you want to copy to the remote_repo
+root_source_dir=json-examples
 
-
-if [ -z "$(git status --porcelain ./json-examples)" ]; then 
+# check status change in root_source_dir
+if [ -z "$(git status --porcelain ./${root_source_dir})" ]; then 
+  # if NO status change do nothing and exit
   echo "Working directory clean"
+  exit 1
 else 
-  # construct branch_name
+  # if YES status change do stuff
+  echo "Changes to be commited"
+
+  # construct a branch_name
   timestamp=$(date +%s)
-
-  branch_prefix=sync-examples
-
+  branch_prefix="sync-examples"
   branch_name="${branch_prefix}--${timestamp}"
-
-  # directory in the project's root that contain the files you want to add to the remote_repo
-  root_source_dir=json-examples
 
   # define a temp_source_dir called branch_name
   temp_source_dir=${branch_name}
 
   # define the remote_repo you will copy files into
-  remote_repo=https://github.com/flavioespinoza/sync-docs.git
+  remote_repo="https://github.com/flavioespinoza/sync-docs.git"
+
+  # define a commit message
+  commit_message="${branch_name} copied to ${remote_repo}"
 
   # remove any existing temp_source_dir that starts with the branch_prefix
   rm -rf ${branch_prefix}*
 
-  # clone the destination_repo into the project's root directory named temp_source_dir
+  # clone the destination_repo as temp_source_dir
   git clone ${remote_repo} ${temp_source_dir}
 
   # cd into the temp_source_dir
@@ -44,21 +49,21 @@ else
   # add changes to staging
   git add .
 
-  # commit changes with a message 
-  git commit -m "${branch_name} to ${remote_repo}"
+  # commit changes to branch_name with commit_message
+  git commit -m ${commit_message}
 
-  # push files to track on remote_repo branch_name
+  # push branch_name upstream to remote_repo
   git push -u origin ${branch_name}
 
   # create a pull-request on remote_repo
-  hub pull-request -h ${branch_name} -m "balls"
+  hub pull-request -h ${branch_name} -m ${commit_message}
 
-  # cd into the project's root directory
+  # cd back into the project root directory
   cd ..
 
   # remove the temp_source_dir
   rm -rf ${temp_source_dir}
 
   # log success
-  echo "Disco! :)"
+  echo "Disco! ${commit_message} was successfull! :)"
 fi
